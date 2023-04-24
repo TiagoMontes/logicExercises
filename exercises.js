83425fde394ad8764359880e10ebf59213a9c249
TiagoMontes <tiagosantos6303@gmail.com>
2023-04-24 17:27:25 -0500

    other way to solve exercise 1

--- exercises.js
+++ exercises.js
@@ -1,23 +1,18 @@
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array. n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm, "nothing" in Erlang).
 
 function consecString(arrayString, consecutive) {
-    const arrayLength = arrayString.arrayLength;
-    const longestString = '';
+    let longest = '';
 
+    // enquanto consecutive for maior que zero (eliminando possibilidades negativas), e i for menor ou igual ao tamanho da array - N cosnecutivos (assim, no caso de você ter N consecutivos maior que a array, ele irá encerrar o looping pois não há valores para calcular a soma dos consecutivos)
+    for (let i = 0; consecutive > 0 && i <= arrayString.length - consecutive; i++) {
+        let currentArray = arrayString.slice(i, i + consecutive).join('');
 
-    if (arrayLength === 0 || consecutive > arrayLength || consecutive <= 0) {
-        return '';
-    }
-
-    for (let i = 0; i <= arrayLength - consecutive; i++){
-        const currentStrings = arrayString.slice(i, i + consecutive).join('');
-
-        if (currentStrings.length > longestString.length) {
-            longestString = currentStrings
+        if (joinStrings.length > longest.length) {
+            longest = currentArray
         }
     }
-
-    return longestString;
+    
+    return longest;
 }
 
-consecString(["zone", "abigail", "theta", "form", "libe", "zas"],3)
+consecString(["zone", "abigail", "theta", "form", "libe", "zas"],5)
